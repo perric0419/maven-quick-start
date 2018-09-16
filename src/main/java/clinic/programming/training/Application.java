4bf48f32a78d8afdbc876d47d2bc69cd88051250
alex.ge <alex.ge@ef.com>
2018-09-16 20:49:38 +0800

    Adding Unit Testing

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,28 +1,35 @@
 package clinic.programming.training;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    
+
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-		Application app = new Application();
-		app.greet();
+    public static void main(String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+        app.greet();
     }
 
     public void greet() {
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
-        for(String greet: greetings) {
+        for (String greet : greetings) {
             System.out.println("Greeting:" + greet);
         }
+
+    }
+
+    public int countWords(String words) {
+        String[] separatedWords = StringUtils.split(words, " ");
+        return (separatedWords == null) ? 0 : separatedWords.length;
 
     }
 }
